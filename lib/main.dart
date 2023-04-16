f60e51009232886d77fcf9db5d78ac56b9c31870
insun <kor.insun@gmail.com>
2023-04-16 22:14:21 +0900

    FIRST COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          useMaterial3: true,
+        ),
+        home: const Scaffold(
+          body: Center(
+            child: Text('Work in Progress'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
